bf02aedb3eec951a0ff13174bc3551aed96ce108
Jason Valdez <jvaldez@ivaldez.net>
2021-07-03 21:53:08 -0400

    Issue-5 Added localconfig file that is designed to allow custom local configuration for databases.

---
+++ src/SharedTestFramework/LocalConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ivaldez.Sql.SharedTestFramework
+{
+    public class LocalConfig
+    {
+        private static object lockObject = new object();
+        private static LocalConfig _instance;
+
+        public static LocalConfig Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (_instance == null)
+                    {
+                        var path = TestUtilities.AssemblyDirectory + @"\..\..\..\..\.localconfig";
+
+                        _instance = TestUtilities.DeserializeFile<LocalConfig>(path);
+                    }
+
+                    return _instance;
+                }
+            }
+        }
+
+        public string DatabaseName { get; set; }
+        public string SqlExpressConnectionString { get; set; }
+        public string SqlExpressMasterConnectionString { get; set; }
+        public string PostgreSqlConnectionString { get; set; }
+        public string PostgreSqlMasterConnectionString { get; set; }
+    }
+}
---
+++ src/SharedTestFramework/TestUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace ivaldez.Sql.SharedTestFramework
+{
+    public class TestUtilities
+    {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public static T DeserializeFile<T>(string file)
+        {
+            var path = TestUtilities.AssemblyDirectory + @"\..\..\..\..\.localconfig";
+
+            var json = File.ReadAllText(path);
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
