4c15611fc2b2faffa94674939309a5a3fa1ffc66
Jason Valdez <jvaldez@ivaldez.net>
2024-05-11 14:16:41 -0400

    Removed working code and ensured that tests were still passing after dependabot bumps.

---
+++ src/IntegrationShared/IterationExtensionTests.cs
@@ -0,0 +1,56 @@
+﻿//using System.Collections.Generic;
+//using FluentAssertions;
+//using ivaldez.Sql.SqlBulkLoader.Core;
+//using Xunit;
+//using Xunit.Abstractions;
+
+//namespace ivaldez.Sql.IntegrationShared
+//{
+//    public class IterationExtensionTests
+//    {
+//        private readonly ITestOutputHelper _output;
+
+//        public IterationExtensionTests(ITestOutputHelper output)
+//        {
+//            _output = output;
+//        }
+
+//        [Fact]
+//        public void ShouldBatchEnumerable()
+//        {
+//            var list = new List<string>()
+//            {
+//                "1", "2", "3", "4", "5", "6",
+//                "1a", "2a", "3a", "4a", "5a", "6a",
+//                "1b", "2b", "3b", "4b", "5b", "6b",
+//                "1c", "2c", "3c", "4c", "5c", "6c"
+//            };
+
+//            var batches = list.Batch(6);
+
+//            var assertionStrings = new[]
+//            {
+//                "1 2 3 4 5 6 ",
+//                "1a 2a 3a 4a 5a 6a ",
+//                "1b 2b 3b 4b 5b 6b ",
+//                "1c 2c 3c 4c 5c 6c "
+//            };
+
+//            var index = 0;
+//            foreach (var batch in batches)
+//            {
+//                var outputLine = "";
+//                foreach (var item in batch)
+//                {
+//                    var iterItem = item;
+
+//                    outputLine += iterItem + " ";
+//                }
+
+//                _output.WriteLine($"{index} - {outputLine}");
+
+//                outputLine.Should().Be(assertionStrings[index++]);
+//            }
+//        }
+//    }
+//}
---
+++ src/ivaldez.SqlBulkLoader.Core/IterationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ivaldez.Sql.SqlBulkLoader.Core
+{
+    public static class IterationExtensions
+    {
+        public static bool IsOver = false; 
+
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> enumerable, int batchSize)
+        {
+            IEnumerator<T> enumerator = enumerable.GetEnumerator();
+
+            var start = true;
+            while (true)
+            {
+                var batchAux = BatchAux<T>(enumerator, batchSize);
+
+                if (IsOver) break;
+                if (enumerator.Current == null && start == false) break;
+                if (start) start = false;
+
+                yield return batchAux;
+            }
+        }
+
+        private static IEnumerable<T> BatchAux<T>(IEnumerator<T> enumerable, int batchSize)
+        {
+            var batchCount = batchSize;
+            while (true)
+            {
+                var hasNextItem = enumerable.MoveNext();
+
+                batchCount--;
+
+                if (!hasNextItem)
+                {
+                    IsOver = true;
+                    break;
+                }
+
+                yield return enumerable.Current;
+
+                if (batchCount <= 0) break;
+            }
+        }
+    }
+}
